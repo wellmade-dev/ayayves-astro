5ff1728e96841c592dd792465101ee28974b2d2b
Samuel @ Wellmade <samuel.broomby@gmail.com>
2024-04-17 16:02:28 +0100

    Update Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  output: "hybrid",
   adapter: cloudflare(),
+  trailingSlash: "never",
 });
