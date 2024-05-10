9af697856721cefc795626d642278fc17adb75b6
Samuel @ Wellmade <samuel.broomby@gmail.com>
2024-05-10 14:42:15 +0100

    Add link prefetching

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,16 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "hybrid",
-  adapter: cloudflare({
-    imageService: "compile",
-  }),
-  trailingSlash: "never",
-  image: {
-    domains: ["storage.ayayves.com"],
-  },
+	output: "hybrid",
+	adapter: cloudflare({
+		imageService: "compile",
+	}),
+	trailingSlash: "never",
+	image: {
+		domains: ["storage.ayayves.com"],
+	},
+	prefetch: {
+		defaultStrategy: "load",
+		prefetchAll: true,
+	},
 });
